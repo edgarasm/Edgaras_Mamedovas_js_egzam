bc82970fc9c9157bf072511947f2c8e37c35bee0
Edgaras <edgaras.mmm@gmail.com>
2022-08-18 14:23:14 +0300

    dog owners array and adults array

--- Task 1/script.js
+++ Task 1/script.js
@@ -8,3 +8,31 @@ pamatyti jo pateikto svorio kovertavimą į:
 Pastaba: atvaizdavimas turi būti matomas pateikus formą ir pateikiamas
 <div id="output"></div> viduje, bei turi turėti bent minimalų stilių;
 ------------------------------------------------------------------- */
+
+const inputEL = document.getElementById('search');
+const submitBtnEl = document.getElementById('submit-btn');
+const outputEl = document.getElementById('output');
+const formEl = document.forms[0];
+
+const kgToLb = inputEL.value * 2.2046;
+const kgToG = inputEL.value / 0.001;
+const kgToOz = inputEL.value * 35.274;
+
+formEl.addEventListener('submit', (e) => {
+  e.preventDefault();
+  showResultInHtml();
+});
+
+function showResultInHtml() {
+  const pEl = document.createElement('p');
+  pEl.innerHTML = `${inputEL.value}kg = ${(inputEL.value * 2.2046).toFixed(
+    2
+  )}lb <br>`;
+  pEl.innerHTML += `${inputEL.value}kg = ${(inputEL.value / 0.001).toFixed(
+    0
+  )}g <br>`;
+  pEl.innerHTML += `${inputEL.value}kg = ${(inputEL.value * 35.274).toFixed(
+    2
+  )}oz`;
+  outputEl.append(pEl);
+}
